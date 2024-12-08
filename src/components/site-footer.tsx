ba942347b296318d247bc1e1e64e67415877e8da
Muahemmed A <muhammed.a.2012.5@gmail.com>
2024-12-08 09:49:46 +0530

    Update site-footer.tsx

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -4,7 +4,7 @@ function SiteFooter() {
   return (
     <footer className=" py-12">
       <div className="border-t border-slate-800 pt-8 flex flex-col items-center">
-    
+    Thanks For Visiting
       </div>
     </footer>
   );
