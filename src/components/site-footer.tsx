013000c665c8d730ebec742148de11c8b331b484
Muahemmed A <muhammed.a.2012.5@gmail.com>
2024-12-08 09:46:31 +0530

    Update site-footer.tsx

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -4,9 +4,7 @@ function SiteFooter() {
   return (
     <footer className=" py-12">
       <div className="border-t border-slate-800 pt-8 flex flex-col items-center">
-        <p className="text-sm text-slate-400 text-center">
-          © 2024 Orbbitt. All rights reserved.
-        </p>
+    
       </div>
     </footer>
   );
