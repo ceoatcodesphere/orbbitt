ced4b43c515d11cd216ca60bac0be7aabc4dff54
Muahemmed A <muhammed.a.2012.5@gmail.com>
2024-12-09 07:01:46 +0530

    Update hero-section.tsx

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -2,25 +2,28 @@ import React from "react";
 
 export function HeroSection() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center text-center px-4">
+    <section className="min-h-screen flex flex-col justify-center items-center text-center px-4">
       {/* Heading Section */}
-      <h2 className="text-4xl w-full max-w-5xl mx-auto z-20 md:text-4xl lg:text-7xl font-bold text-white font-sans tracking-tight mb-28">
-        Connecting{" "}
-        <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500">
-          Communities
-        </span>
-        , <br /> Building Careers.
-      </h2>
+      <header className="mb-28">
+        <h2 className="text-white font-bold text-4xl md:text-5xl lg:text-7xl tracking-tight w-full max-w-5xl mx-auto">
+          Connecting <span className="bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 bg-clip-text text-transparent">
+                    Communities
+          </span>
+          , <br /> Building Careers.
+        </h2>
+      </header>
 
       {/* Button Section */}
-      <a href="/search">
-        <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 mb-48">
-          <span className="absolute inset-[-1000%] animate-spin bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-          <span className="inline-flex h-full w-full items-center justify-center rounded-full bg-slate-950 px-16 py-5 text-sm font-medium text-white backdrop-blur-3xl">
-            Get Started
-          </span>
-        </button>
-      </a>
-    </div>
+      <div className="mb-48">
+        <a href="/search" className="group inline-flex">
+          <div className="relative rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+            <div className="" />
+            <div className="relative border border-zinc-100 flex items-center justify-center px-16 py-3 rounded-full bg-slate-950">
+              <span className="text-white text-sm font-medium">Get Started</span>
+            </div>
+          </div>
+        </a>
+      </div>
+    </section>
   );
 }
