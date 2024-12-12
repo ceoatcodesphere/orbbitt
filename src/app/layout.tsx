b92852903858233facc76a623e3b55bbfaaa44e1
Muahemmed A <muhammed.a.2012.5@gmail.com>
2024-12-12 07:16:29 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,7 @@ const geistMono = localFont({
 });
 export const metadata: Metadata = {
   title: "Orbbitt",
-  description: "Connecting Communities,
-Building Careers",
+  description: "Connecting Communities, Building Careers",
   
 };
 
@@ -32,8 +31,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} dark antialiased bg-gradient-to-r font-sans from-gray-900 via-gray-800 to-black`}
       >
           <meta name="description" content="Connecting Communities,
-Building Careers.">
-  <meta name="keywords" content="orbit, orbbitt, orbbitt.in, education, college finder">
+Building Careers." />
+  <meta name="keywords" content="orbit, orbbitt, orbbitt.in, education, college finder" />
         <meta name="google-site-verification" content="8E4pLYMXLT2QnpN2tQNiL9Jd6pV99KhzQLMrsFG4Vkk" />
         <meta name="google-adsense-account" content="ca-pub-1275542292926934"></meta>
         <link rel="shortcut icon" href="/public/28314227_6-ai.svg" type="image/x-icon" />
