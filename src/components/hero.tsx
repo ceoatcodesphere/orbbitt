9f18e40865eff95a500e8bc978ca406ae34703b1
Muahemmed A <muhammed.a.2012.5@gmail.com>
2024-12-13 07:03:11 +0530

    Update hero.tsx

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -20,13 +20,13 @@ export function GridBackgroundDemo() {
           </button>
         </a>
       </div>
-      <h1 className="font-bold text-xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4 mb-16">
+      <h1 className="font-bold text-4xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4 mb-16">
         Connecting{" "}
         <span className="">
           Communities
         </span>
         <br />
-        <span className="mt-4 block">Building Careers.</span>
+        Building Careers.
       </h1>
       
       <div className="flex justify-between items-center p-2 bg-gray-700 rounded-full max-w-lg w-full mx-auto text-white shadow-md">
@@ -35,9 +35,9 @@ export function GridBackgroundDemo() {
          placeholder="Search by course name..."
         className="flex-grow bg-transparent border-none text-sm sm:text-base placeholder-gray-400 focus:outline-none text-white"
       />
-      <a href="/auth">
+      <a href="/search">
       <button className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-full text-sm sm:text-base transition-all">
-        Get Started →
+      Go →
       </button>
       </a>
     </div>
